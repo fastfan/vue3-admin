b5398a2d827d02691278e957014170f2c318292e
fanwei <fanwei@citycloud.com.cn>
2025-02-09 00:03:28 +0800

    feat(*): 新增配置文件

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: fastfan
+ * @Date: 2025-02-08 22:50:58
+ * @LastEditors: fastfan
+ * @LastEditTime: 2025-02-08 22:50:59
+ * @Description: your description
+ */
+module.exports = {
+    printWidth: 120, //单行长度
+    tabWidth: 4, //缩进长度
+    useTabs: false, //使用空格代替tab缩进
+    semi: true, //句末使用分号
+    singleQuote: true, //使用单引号
+    endOfLine: 'auto',
+    // indent: 4, //缩进长度
+    trailingComma: 'none' // 对象最后一个属性末尾是否要逗号
+};
